ffbf7d9b51f79eed2e1dae5288588c73c6478b2d
bambinexx <bambinex456@gmail.com>
2025-09-29 10:53:21 +0200

    get basic pygame working

---
+++ main.py
@@ -0,0 +1,25 @@
+# Example file showing a basic pygame "game loop"
+import pygame
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((1280, 720))
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill("black")
+
+        pygame.display.flip()
+
+        clock.tick(60)
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
