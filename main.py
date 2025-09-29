4ac06e10648912a5b486534305f246d06539cf1e
bambinexx <bambinex456@gmail.com>
2025-09-29 11:23:25 +0200

    simple grid display

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from src.display import init
+import pygame
+from src.display import display_grid
 
 def main():
-    init()
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    clock = pygame.time.Clock()
+    running = True
+
+    grid : set = set()
+    coordinates : list = [0,0]
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        display_grid(grid, screen, coordinates)
+
+        clock.tick(60)
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
--- src/display.py
+++ src/display.py
@@ -1,20 +1,24 @@
 import pygame
 
-def init():
-    pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
-    clock = pygame.time.Clock()
-    running = True
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        screen.fill("black")
-
-        pygame.display.flip()
-
-        clock.tick(60)
-
-    pygame.quit()
+def display_grid(grid: set,
+                  screen: pygame.surface.Surface, 
+                  coords: list) -> None:
+    
+    for i in range(screen.get_width() // 20):
+        for j in range(screen.get_height() // 20):
+            if (coords[0] + i, coords[1] + j) in grid:
+                pygame.draw.rect(screen, 
+                                 "white", 
+                                 pygame.Rect((coords[0] + i)*20, (coords[1] + j)*20, 20, 20))
+            else:
+                pygame.draw.rect(screen, 
+                                 "black", 
+                                 pygame.Rect((coords[0] + i)*20, (coords[1] + j)*20, 20, 20))
+    
+    for i in range(screen.get_width() // 20):
+        pygame.draw.line(screen, "white", (i*20, 0), (i*20, screen.get_width()))
+    
+    for i in range(screen.get_height() // 20):
+        pygame.draw.line(screen, "white", (0, i*20), (screen.get_width(), i*20))
+    
+    pygame.display.flip()
